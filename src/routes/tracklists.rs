3a78e54878d27a99d7fea3c6eaae29f29d6677fd
Wollaston <mattflynn1997@gmail.com>
2024-02-02 11:43:55 -0500

    Add create artist functionality

--- src/routes/tracklists.rs
+++ src/routes/tracklists.rs
@@ -1,6 +1,9 @@
 use askama::Template;
 use askama_axum::IntoResponse;
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use sqlx::SqlitePool;
 
 use crate::routes;
@@ -20,8 +23,13 @@ pub async fn tracklists() -> impl IntoResponse {
 
 pub fn tracklists_routes() -> Router<SqlitePool> {
     Router::new()
-        .route("/artists", get(routes::tracklists::artists::artists))
-        .route("/artists/:id", get(routes::tracklists::artists::artist))
+        .route("/artists", get(routes::tracklists::artists::get_artists))
+        .route(
+            "/artists/create",
+            get(routes::tracklists::artists::create_artist)
+                .post(routes::tracklists::artists::post_artist),
+        )
+        .route("/artists/:id", get(routes::tracklists::artists::get_artist))
         .route(
             "/mix-series",
             get(routes::tracklists::mix_series::mix_series),
--- src/routes/tracklists/artists.rs
+++ src/routes/tracklists/artists.rs
@@ -1,6 +1,9 @@
 use askama::Template;
 use askama_axum::IntoResponse;
-use axum::extract::{Path, State};
+use axum::{
+    extract::{Path, State},
+    Form,
+};
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
 
@@ -22,14 +25,24 @@ pub struct ArtistTemplate {
     artist: Artist,
 }
 
-#[derive(sqlx::FromRow, Serialize, Deserialize)]
+#[derive(Template)]
+#[template(path = "routes/tracklists/create_artist.html")]
+pub struct CreateArtistTemplate;
+
+#[derive(Debug, sqlx::FromRow, Serialize, Deserialize)]
 pub struct Artist {
     pub artist_id: i64,
     pub artist_name: String,
     pub real_name: Option<String>,
 }
 
-pub async fn artist(State(pool): State<SqlitePool>, Path(id): Path<i64>) -> impl IntoResponse {
+#[derive(Debug, Deserialize)]
+pub struct CreateArtist {
+    pub artist_name: String,
+    pub real_name: Option<String>,
+}
+
+pub async fn get_artist(State(pool): State<SqlitePool>, Path(id): Path<i64>) -> impl IntoResponse {
     let artist = sqlx::query_as::<_, Artist>(
         "
     SELECT * FROM artists WHERE artist_id = $1
@@ -43,7 +56,32 @@ pub async fn artist(State(pool): State<SqlitePool>, Path(id): Path<i64>) -> impl
     ArtistTemplate { artist }
 }
 
-pub async fn artists(State(pool): State<SqlitePool>) -> impl IntoResponse {
+pub async fn post_artist(
+    State(pool): State<SqlitePool>,
+    Form(input): Form<CreateArtist>,
+) -> impl IntoResponse {
+    let id = sqlx::query(
+        "
+    INSERT INTO artists (artist_name, real_name)
+    values($1, $2)
+RETURNING *
+    ",
+    )
+    .bind(input.artist_name)
+    .bind(input.real_name.unwrap())
+    .execute(&pool)
+    .await
+    .unwrap()
+    .last_insert_rowid();
+
+    get_artist(axum::extract::State(pool), axum::extract::Path(id)).await
+}
+
+pub async fn create_artist() -> impl IntoResponse {
+    CreateArtistTemplate
+}
+
+pub async fn get_artists(State(pool): State<SqlitePool>) -> impl IntoResponse {
     let artists = sqlx::query_as::<_, Artist>(
         "
     SELECT * FROM artists
