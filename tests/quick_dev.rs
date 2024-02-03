478e1bb013726083fe3f14f49311cd4ad005eac1
Wollaston <mattflynn1997@gmail.com>
2024-02-02 21:59:22 -0500

    Add test for dummy login

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -1,10 +1,18 @@
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
     hc.do_get("/").await?.print_no_body().await?;
+
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({"username" : "demo1", "password": "welcome"}),
+    );
+
+    req_login.await?.print().await?;
 
     Ok(())
 }
