16d1cf5ac563f58604bfd8cbca291201a01ddbc2
Wollaston <mattflynn1997@gmail.com>
2024-02-04 21:38:06 -0500

    Add client side errors and map to server errors

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,7 @@ use axum::http::StatusCode;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
     DBError,
@@ -18,7 +18,31 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+            // -- Auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInReqExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+            // -- Model
+            Self::DBError => (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS),
+            // -- Fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
     }
 }
 
@@ -29,3 +53,12 @@ impl std::fmt::Display for Error {
 }
 
 impl std::error::Error for Error {}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use axum::{extract::FromRef, middleware, response::Response, Router};
+use askama_axum::IntoResponse;
+use axum::{
+    extract::FromRef,
+    middleware,
+    response::{self, Response},
+    Json, Router,
+};
 use model::ModelController;
+use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::{
     services::{ServeDir, ServeFile},
@@ -55,7 +62,31 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
-    println!();
+    let uuid = uuid::Uuid::new_v4();
 
-    res
+    // Get Server Error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // If Client Error, build the response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error" : {
+                "type": client_error.as_ref(),
+                "req_uuid": uuid.to_string(),
+            }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // Build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}");
+
+    println!();
+    error_response.unwrap_or(res)
 }
